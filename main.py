cfa3e7fc48c147d5e13aabf2891305d018310076
Isabela Escalante <isaescalante96@Isas-MacBook-Air-2.local>
2017-11-18 16:09:48 -0600

    input checa errores pero no I/O

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ procesoID = []
 arrival_time = []
 execution_time = []
 io_dict = dict()
+error_procesos = False
 
 with open("cases.txt") as archivo_entrada :
     for line in archivo_entrada :
@@ -21,13 +22,17 @@ with open("cases.txt") as archivo_entrada :
         if words[0] == "CPUS" :
             cpus = int(words[1])
         if words[0].isdigit() :
-            
-            procesoID.append(int(words[0]))
-            arrival_time.append(int(words[1]))
-            execution_time.append(int(words[2]))
-            #if words[3] == "I/O" :
+            if len(words) < 3 :
+                error_procesos = True
+            if len(words) == 3 :
+                procesoID.append(int(words[0]))
+                arrival_time.append(int(words[1]))
+                execution_time.append(int(words[2]))
+            if len(words) > 3 :
+                if words[3] != "I/O" :
+                    error_procesos = True
         if words[0] == "FIN" :
-            if politica == "-1" or quantum <> 0 or context_switch == -1 or cpus == 0) : 
+            if politica == "-1" or quantum <> 0 or context_switch == -1 or cpus == 0 or error_procesos or len(procesoID) == 0  : 
                 print "error"
             else :
                 print "correct"
