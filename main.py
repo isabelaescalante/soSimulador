ab1e87c6670ff890edbb11a585853df01530413a
Isabela Escalante <isaescalante96@Isas-MacBook-Air-2.local>
2017-11-24 13:40:54 -0600

    con el input correcto y funciones de print

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 #from simulador import SJF, SRT
+class Proceso:
+    def __init__(self, i_d, at, et, i_o=[]):
+        self.id = i_d
+        self.arr_time = at
+        self.exe_time = et
+        self.io = i_o
+        self.wait_time = 0
+        self.end_time = 0
+
 
 politica = "-1"
 quantum = -1
 context_switch = -1
 cpus = 0
-procesoID = []
-arrival_time = []
-execution_time = []
-io_dict = dict()
 error_procesos = False
+lista_procesos = []
+
 
 with open("cases.txt") as archivo_entrada :
     for line in archivo_entrada :
@@ -25,25 +32,35 @@ with open("cases.txt") as archivo_entrada :
             if len(words) < 3 :
                 error_procesos = True
             if len(words) == 3 :
-                procesoID.append(int(words[0]))
-                arrival_time.append(int(words[1]))
-                execution_time.append(int(words[2]))
+                if words[2] == 0 :
+                    error_procesos = True
+                else :
+                    proceso = Proceso(int(words[0]), int(words[1]), int(words[2]))
+                    lista_procesos.append(proceso)
             if len(words) > 3 :
                 if words[3] != "I/O" :
                     error_procesos = True
+                    proceso = Proceso(int(words[0]), int(words[1]), int(words[2]))
+                    lista_procesos.append(proceso)
         if words[0] == "FIN" :
-            if politica == "-1" or quantum <> 0 or context_switch == -1 or cpus == 0 or error_procesos or len(procesoID) == 0  : 
+            if politica == "-1" or quantum <> 0 or context_switch == -1 or cpus == 0 or error_procesos or len(lista_procesos) == 0  : 
                 print "error"
             else :
                 print "correct"
+            
+            #imprime input
+            print politica
+            print quantum
+            print context_switch
+            print cpus
+            print procesoID
+            print arrival_time 
+            print execution_time
 
-
-
-
-print politica
-print quantum
-print context_switch
-print cpus
-print procesoID
-print arrival_time 
-print execution_time
+            #inicia los valores otra vez para siguiente simulación
+            politica = "-1"
+            quantum = -1
+            context_switch = -1
+            cpus = 0
+            error_procesos = False
+            lista_procesos = []
--- simulador.py
+++ simulador.py
@@ -1,5 +1,32 @@
-def SRT() :
+#imprime el tiempo, la cola de listas, cpu y procesos bloqueados cuando hay cambios durante la simulación
+def printChanges() :
+    print "Tiempo\tCola de listas\t",
+    for i in range(1,cpus+1) :
+        print "\tCPU" + str(i) + "\t" ,
+    print "Procesos bloqueados"
+
+    print processID,"\t\t",2,"\t\t",str(20),"\t\t",str(8),"\t\t",str(5)
+
+#imprime los tiempos de turnaround y espera de los procesos con su promedio al final de la simulación
+def printFinal() :
+    turnarounds = [4,2]
+    espera = [3,5]
+    promedioTurnaround = 0
+    promedioEspera = 0
+    proceso = 2 
+    print "Proceso\t\tTiempo de turnaround\tTiempo de espera"
+    for x in range(1,proceso+1) :
+        print str(x),"\t\t\t",str(turnarounds[x-1]),"\t\t\t",str(espera[x-1])
+
+    print "Promedio de turnaround: ",
+    print (sum(turnarounds) / proceso)
+    print "Promedio de espera: ",
+    print (sum(espera) / proceso)
+
+#def SRT() :
 
 
-def SJF() :
+#def SJF() :
 
+
+printFinal()
