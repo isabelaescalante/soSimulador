f7818df7927a326ba735c39b2d130f06138931bb
joseasalazar <joseandresss@hotmail.com>
2017-11-25 16:22:20 -0600

    primera parte completa

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ class Proceso:
         self.wait_time = 0
         self.end_time = 0
 
-
 politica = "-1"
 quantum = -1
 context_switch = -1
 cpus = 0
-error_procesos = False
+flag = False
 lista_procesos = []
-
+io = []
 
 with open("cases.txt") as archivo_entrada :
     for line in archivo_entrada :
@@ -24,28 +23,29 @@ with open("cases.txt") as archivo_entrada :
             politica = words[0]
         if words[0] == "QUANTUM" :
             quantum = int(words[1])
-        if words[0] == "CONTEXT" :
+        if words[0] == "CONTEXT" and words[1] == "SWITCH":
             context_switch =  int(words[2])
         if words[0] == "CPUS" :
             cpus = int(words[1])
             # falta romper todo
         if words[0].isdigit() :
-            if len(words) < 3 :
-                error_procesos = True
             if len(words) == 3 :
                 proceso = Proceso(int(words[0]), int(words[1]), int(words[2]))
                 lista_procesos.append(proceso)
             if len(words) > 3 :
-                if words[3] != "I/O" :
-                    error_procesos = True
-                    proceso = Proceso(int(words[0]), int(words[1]), int(words[2]))
+                if words[3] == "I/O" :
+                    for i in words[4:] :
+                        io.append(int(i))
+                    proceso = Proceso(int(words[0]), int(words[1]), int(words[2]), io)
                     lista_procesos.append(proceso)
-        #if words[0] == "FIN" :
-            #if politica == "-1" or quantum <> 0 or context_switch == -1 or cpus == 0 or error_procesos or len(procesoID) == 0  : 
-            #    print "error"
-            #else :
-             #   print "correct"
+        if words[0] == "FIN" :
+            if politica == "-1" or quantum <> 0 or context_switch == -1 or cpus == 0 : 
+                flag = False
+            else :
+                flag = True
 
+lista_procesos.sort(key=lambda x: (x.arr_time, x.exe_time), reverse=False)
 
-print(lista_procesos[0].id, lista_procesos[0].arr_time, lista_procesos[0].exe_time)
-print(lista_procesos[1].id, lista_procesos[1].arr_time, lista_procesos[1].exe_time)
+if flag :
+    for i in lista_procesos :
+        print(i.id, i.arr_time, i.exe_time)
